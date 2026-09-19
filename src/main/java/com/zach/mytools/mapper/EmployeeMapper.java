2687582ca5c0243c15301acd572ef645b30acb3b
zachshen777 <15052379214@163.com>
2026-09-19 09:00:30 +0800

    feat:定时任务

--- src/main/java/com/zach/mytools/mapper/EmployeeMapper.java
+++ src/main/java/com/zach/mytools/mapper/EmployeeMapper.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 /**
  * 员工信息 Mapper（MyBatis-Plus）
  */
--- src/main/java/com/zach/mytools/service/SysDictService.java
+++ src/main/java/com/zach/mytools/service/SysDictService.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
